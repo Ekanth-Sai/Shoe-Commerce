c92af87c37adfbb60de9e07c24f05fb8cbbba2b2
Ekanth-Sai <ekanthsai.yellanki@gmail.com>
2023-12-26 22:25:09 +0530

    Video 8

--- Admin/index.php
+++ Admin/index.php
@@ -7,6 +7,11 @@
     <!-- Bootstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+
 <!-- CSS file -->
 <link rel="stylesheet" href="style.css">
 </head>
@@ -42,9 +47,9 @@
                 <div class="button text-center">
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Producs</a></button>
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Producs</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
+                    <button class="my-3"><a href="index.php?insert_category" class="nav-link text-light bg-info my-1">Insert Categories</a></button>
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
+                    <button class="my-3"><a href="index.php?insert_brand" class="nav-link text-light bg-info my-1">Insert Brands</a></button>
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">View Brands</a></button>
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Orders</a></button>
                     <button class="my-3"><a href="" class="nav-link text-light bg-info my-1">All Payments</a></button>
@@ -53,6 +58,17 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <!-- fourth child -->
+    <div class="container my-5">
+        <?php
+        if(isset($_GET['insert_category'])){
+            include('insert_categories.php');
+        }
+        if(isset($_GET['insert_brand'])){
+            include('insert_brands.php');
+        }?>
     </div>
 
     <!-- last child -->
